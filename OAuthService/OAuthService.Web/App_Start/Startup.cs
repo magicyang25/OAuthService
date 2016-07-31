492d279370cc0ad13a15ce41eb2b4ac35395bbdc
unknown <yangyuxiang0925@gmail.com>
2016-08-01 00:13:19 +1000

    appsettings and startup

---
+++ OAuthService/OAuthService.Web/App_Start/Startup.cs
@@ -0,0 +1,49 @@
+﻿using IdentityServer3.Core.Configuration;
+using Microsoft.Owin;
+using Owin;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+[assembly: OwinStartup(typeof(OAuthService.Web.Startup))]
+
+namespace OAuthService.Web
+{
+    public class Startup
+    {
+        private bool _isProduction
+        {
+            get
+            {
+                var result = true;
+                #if DEBUG
+                result = false;
+                #endif
+                return result;
+            }
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            var signingCertificateFileName = ConfigurationManager.AppSettings["SigningCertificateFileName"];
+            var signingCertificatePassword = ConfigurationManager.AppSettings["SigningCertificatePassword"];
+
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            /*
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(InMemoryManager.GetUsers())
+                .UseInMemoryScopes(InMemoryManager.GetScopes())
+                .UseInMemoryClients(InMemoryManager.GetClients());
+            */
+            var ops = new IdentityServerOptions
+            {
+                SigningCertificate = new X509Certificate2(signingCertificateFileName, signingCertificatePassword),
+                RequireSsl = _isProduction,//should be true on production serversss
+                //Factory = factory
+            };
+
+            app.UseIdentityServer(ops);
+        }
+    }
+}
